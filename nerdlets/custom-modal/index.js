91cd658877450d53b550f449d6918ffa73428c8c
Kav91 <kav.pather@gmail.com>
2021-07-28 20:11:46 +1000

    feat: use platformState timeRange

--- nerdlets/custom-modal/index.js
+++ nerdlets/custom-modal/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import {
   AutoSizer,
+  PlatformStateContext,
   NerdletStateContext,
   HeadingText,
   LineChart,
@@ -179,189 +180,196 @@ export default class CustomModalNerdlet extends React.Component {
     return (
       <AutoSizer>
         {({ width, height }) => (
-          <NerdletStateContext.Consumer>
-            {nerdletState => {
-              const { queries, accountId, timeRange } = nerdletState;
-              const { time } = this.state;
-              const timeOptions = [
-                {
-                  key: 'platform',
-                  label: 'Platform time selection',
-                  text: 'platform',
-                  value: { ...timeRange }
-                },
-                {
-                  key: '5',
-                  label: '5 minutes',
-                  text: '5',
-                  value: {
-                    begin_time: null,
-                    duration: 5 * MINUTE,
-                    end_time: null
-                  }
-                },
-                {
-                  key: '15',
-                  label: '15 minutes',
-                  text: '15',
-                  value: {
-                    begin_time: null,
-                    duration: 15 * MINUTE,
-                    end_time: null
-                  }
-                },
-                {
-                  key: '30',
-                  label: '30 minutes',
-                  text: '30',
-                  value: {
-                    begin_time: null,
-                    duration: 30 * MINUTE,
-                    end_time: null
-                  }
-                },
-                {
-                  key: '60',
-                  label: '60 minutes',
-                  text: '60',
-                  value: {
-                    begin_time: null,
-                    duration: 60 * MINUTE,
-                    end_time: null
-                  }
-                },
-                {
-                  key: '3',
-                  label: '3 hours',
-                  text: '3',
-                  value: {
-                    begin_time: null,
-                    duration: 3 * HOUR,
-                    end_time: null
-                  }
-                },
-                {
-                  key: '6',
-                  label: '6 hours',
-                  text: '6',
-                  value: {
-                    begin_time: null,
-                    duration: 6 * HOUR,
-                    end_time: null
-                  }
-                },
-                {
-                  key: '12',
-                  label: '12 hours',
-                  text: '12',
-                  value: {
-                    begin_time: null,
-                    duration: 12 * HOUR,
-                    end_time: null
-                  }
-                },
-                {
-                  key: '24',
-                  label: '24 hours',
-                  text: '24',
-                  value: {
-                    begin_time: null,
-                    duration: 24 * HOUR,
-                    end_time: null
-                  }
-                },
-                {
-                  key: '3d',
-                  label: '3 days',
-                  text: '3d',
-                  value: {
-                    begin_time: null,
-                    duration: 3 * DAY,
-                    end_time: null
-                  }
-                },
-                {
-                  key: '7d',
-                  label: '7 days',
-                  text: '7d',
-                  value: {
-                    begin_time: null,
-                    duration: 7 * DAY,
-                    end_time: null
-                  }
-                }
-              ];
-              const selectedTime = time || timeOptions[0];
-              width = width * 0.95;
+          <PlatformStateContext.Consumer>
+            {platformState => (
+              <NerdletStateContext.Consumer>
+                {nerdletState => {
+                  const { queries, accountId } = nerdletState;
+                  const { timeRange } = platformState;
+                  const { time } = this.state;
+                  const timeOptions = [
+                    {
+                      key: 'platform',
+                      label: 'Platform time selection',
+                      text: 'platform',
+                      value: { ...timeRange }
+                    },
+                    {
+                      key: '5',
+                      label: '5 minutes',
+                      text: '5',
+                      value: {
+                        begin_time: null,
+                        duration: 5 * MINUTE,
+                        end_time: null
+                      }
+                    },
+                    {
+                      key: '15',
+                      label: '15 minutes',
+                      text: '15',
+                      value: {
+                        begin_time: null,
+                        duration: 15 * MINUTE,
+                        end_time: null
+                      }
+                    },
+                    {
+                      key: '30',
+                      label: '30 minutes',
+                      text: '30',
+                      value: {
+                        begin_time: null,
+                        duration: 30 * MINUTE,
+                        end_time: null
+                      }
+                    },
+                    {
+                      key: '60',
+                      label: '60 minutes',
+                      text: '60',
+                      value: {
+                        begin_time: null,
+                        duration: 60 * MINUTE,
+                        end_time: null
+                      }
+                    },
+                    {
+                      key: '3',
+                      label: '3 hours',
+                      text: '3',
+                      value: {
+                        begin_time: null,
+                        duration: 3 * HOUR,
+                        end_time: null
+                      }
+                    },
+                    {
+                      key: '6',
+                      label: '6 hours',
+                      text: '6',
+                      value: {
+                        begin_time: null,
+                        duration: 6 * HOUR,
+                        end_time: null
+                      }
+                    },
+                    {
+                      key: '12',
+                      label: '12 hours',
+                      text: '12',
+                      value: {
+                        begin_time: null,
+                        duration: 12 * HOUR,
+                        end_time: null
+                      }
+                    },
+                    {
+                      key: '24',
+                      label: '24 hours',
+                      text: '24',
+                      value: {
+                        begin_time: null,
+                        duration: 24 * HOUR,
+                        end_time: null
+                      }
+                    },
+                    {
+                      key: '3d',
+                      label: '3 days',
+                      text: '3d',
+                      value: {
+                        begin_time: null,
+                        duration: 3 * DAY,
+                        end_time: null
+                      }
+                    },
+                    {
+                      key: '7d',
+                      label: '7 days',
+                      text: '7d',
+                      value: {
+                        begin_time: null,
+                        duration: 7 * DAY,
+                        end_time: null
+                      }
+                    }
+                  ];
+                  const selectedTime = time || timeOptions[0];
+                  width = width * 0.95;
 
-              let widgetsPerCol = 3;
-              if (queries.length === 2) widgetsPerCol = 2;
+                  let widgetsPerCol = 3;
+                  if (queries.length === 2) widgetsPerCol = 2;
 
-              let widgetsPerRow = 3;
-              if (queries.length === 2) widgetsPerRow = 2;
+                  let widgetsPerRow = 3;
+                  if (queries.length === 2) widgetsPerRow = 2;
 
-              return (
-                <div style={{ padding: '10px' }}>
-                  <div className="utility-bar">
-                    <div className="react-select-input-group">
-                      <label>Time Picker</label>
-                      <Select
-                        options={timeOptions}
-                        placeholder="Select a time..."
-                        isClearable
-                        onChange={time => {
-                          this.setState({ time }, () =>
-                            platform.setUrlState({ timeRange: time.value })
-                          );
-                        }}
-                        value={selectedTime}
-                        classNamePrefix="react-select"
-                      />
-                    </div>
-                  </div>
-
-                  {(queries || []).map((q, i) => {
-                    const widgetHeight =
-                      q.height && !isNaN(q.height)
-                        ? `${q.height}px`
-                        : undefined;
-                    const widgetWidth =
-                      q.width && !isNaN(q.width) ? `${q.width}px` : undefined;
-
-                    return (
-                      <div
-                        key={i}
-                        style={{
-                          padding: '7px',
-                          paddingBottom: '50px',
-                          float: 'left',
-                          height: widgetHeight || height / widgetsPerRow,
-                          width: widgetWidth || width / widgetsPerCol
-                        }}
-                      >
-                        {!q.hideTitle && (
-                          <HeadingText
-                            type={HeadingText.TYPE.HEADING_4}
-                            style={{ paddingBottom: '5px' }}
-                          >
-                            {q.chartTitle}
-                          </HeadingText>
-                        )}
-                        {this.renderChart(
-                          accountId,
-                          q.chartType,
-                          q.query,
-                          height,
-                          width,
-                          selectedTime
-                        )}
+                  return (
+                    <div style={{ padding: '10px' }}>
+                      <div className="utility-bar">
+                        <div className="react-select-input-group">
+                          <label>Time Picker</label>
+                          <Select
+                            options={timeOptions}
+                            placeholder="Select a time..."
+                            isClearable
+                            onChange={time => {
+                              this.setState({ time }, () =>
+                                platform.setUrlState({ timeRange: time.value })
+                              );
+                            }}
+                            value={selectedTime}
+                            classNamePrefix="react-select"
+                          />
+                        </div>
                       </div>
-                    );
-                  })}
-                </div>
-              );
-            }}
-          </NerdletStateContext.Consumer>
+
+                      {(queries || []).map((q, i) => {
+                        const widgetHeight =
+                          q.height && !isNaN(q.height)
+                            ? `${q.height}px`
+                            : undefined;
+                        const widgetWidth =
+                          q.width && !isNaN(q.width)
+                            ? `${q.width}px`
+                            : undefined;
+
+                        return (
+                          <div
+                            key={i}
+                            style={{
+                              padding: '7px',
+                              paddingBottom: '50px',
+                              float: 'left',
+                              height: widgetHeight || height / widgetsPerRow,
+                              width: widgetWidth || width / widgetsPerCol
+                            }}
+                          >
+                            {!q.hideTitle && (
+                              <HeadingText
+                                type={HeadingText.TYPE.HEADING_4}
+                                style={{ paddingBottom: '5px' }}
+                              >
+                                {q.chartTitle}
+                              </HeadingText>
+                            )}
+                            {this.renderChart(
+                              accountId,
+                              q.chartType,
+                              q.query,
+                              height,
+                              width,
+                              selectedTime
+                            )}
+                          </div>
+                        );
+                      })}
+                    </div>
+                  );
+                }}
+              </NerdletStateContext.Consumer>
+            )}
+          </PlatformStateContext.Consumer>
         )}
       </AutoSizer>
     );
